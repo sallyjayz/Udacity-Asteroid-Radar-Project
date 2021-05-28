4c2f00f8336a8b508e6405f922ecb7ea7031680d
sallyjayz <salama.jatau@yahoo.com>
2021-05-28 21:49:50 +0100

    removed apikey string from constants file

--- app/src/main/java/com/sallyjayz/asteroidradar/Constants.kt
+++ app/src/main/java/com/sallyjayz/asteroidradar/Constants.kt
@@ -5,7 +5,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-//    const val API_KEY = "eHhmHhkD2nbORow3eHAQlRa6Ehd2kV3vDhfxqgRP"
     var api_key: String = BuildConfig.API_KEY
     const val DATABASE_NAME = "asteroid_database"
 }
